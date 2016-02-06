ce6a25cec73a5af68abff63f696be76ac2f43d5a
Louis Orleans <louis@orleans.io>
2016-02-06 15:56:50 -0800

    stuff

--- modules/credentials.js
+++ modules/credentials.js
@@ -1,15 +1,5 @@
 var fs = require('fs');
 
 module.exports = {
-    apiKey: {
-        root: 'keyboard_cat'
-    },
-    TLScertificate: {
-        certificate: fs.readFileSync('./modules/certificates/cert.pem'),
-        privateKey: fs.readFileSync('./modules/certificates/key.pem')
-    },
-    mongodb: {
-        username: 'TbtMHajmTBRwChbGYKCf',
-        password: 'qUEntm34aRjDaxtuFdjC'
-    }
+
 };
